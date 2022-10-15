e7118deff4d4b5f38b3578346d0612d8fede13f8
JCAmorim23 <juliana.carol.amorim@gmail.com>
2022-10-15 02:11:03 -0300

    Props e elementos custom

--- desafioUm/src/Components/TituloDinamico.js
+++ desafioUm/src/Components/TituloDinamico.js
@@ -1,25 +1,25 @@
 class TituloDinamico extends HTMLElement {
-	constructor(){ 
-		super();
-		const shadow = this.attachShadow({mode: "open"})
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
 
-		/*1 - base do component*/
-		const componentRoot = document.createElement("h1") 
-		componentRoot.textContent = "live Nation";
+    /*1 - base do component*/
+    const componentRoot = document.createElement("h1");
+    componentRoot.textContent = this.getAttribute("love");
+    //        componentRoot.textContent = "";
 
-		/*2 - estilizar */
-		const style = document.createElement("style");
-		style.textContent =  `
+    /*2 - estilizar */
+    const style = document.createElement("style");
+    style.textContent = `
 			h1{
 				color:red;
 			}
 		`;
 
-		/*3 - enviar para a shadow*/
-		shadow.appendChild(componentRoot);
-		shadow.appendChild(style);
-	
-	} 
+    /*3 - enviar para a shadow*/
+    shadow.appendChild(componentRoot);
+    shadow.appendChild(style);
+  }
 }
 
-customElements.define('titulo-dinamico', TituloDinamico);
+customElements.define("titulo-dinamico", TituloDinamico);
