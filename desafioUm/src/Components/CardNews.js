5122d57b60374d2e446d6fbc1193a3f7dd096d4f
JCAmorim23 <juliana.carol.amorim@gmail.com>
2022-10-16 10:57:26 -0300

    components

--- desafioUm/src/Components/CardNews.js
+++ desafioUm/src/Components/CardNews.js
@@ -1,16 +1,19 @@
-class CardNews extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = "Hello World";
-  }
+class Cardnews extends HTMLElement {
+	constructor(){
+		super();
+
+		const shadow = this.attachShadow({mode:"open"});
+		shadow.innerHTML = "<h1>hello</h1>";
+	}
+
+	build(){ 
+	}
+
+	styles(){ 
+	}
 }
 
-customElements.define("card-news", CardNews);
-
-
-
-
+customElements.define("card-news", Cardnews);
 
 
 
