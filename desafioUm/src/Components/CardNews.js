8b5873e78e3dea51df981aeac793d9eb68a5ef3d
JCAmorim23 <juliana.carol.amorim@gmail.com>
2022-10-18 18:41:12 -0300

    Criando atributos no build da árvore fantasma

--- desafioUm/src/Components/CardNews.js
+++ desafioUm/src/Components/CardNews.js
@@ -3,11 +3,38 @@ class Cardnews extends HTMLElement {
 		super();
 
 		const shadow = this.attachShadow({mode:"open"});
-		shadow.innerHTML = "<h1>hello</h1>";
+		shadow.appendChild(this.build());
+	//	shadow.appendChild(this.styles());
 	}
 
 	build(){ 
+		const componentRoot = document.createElement("div");
+		componentRoot.setAttribute("class", "card");
+		const cardLeft = document.createElement("div");
+		cardLeft.setAttribute("class", "card__left");
+		
+		const autor = document.createElement("span");
+		autor.textContent = "By " + (this.getAttribute("autor") || "By Anonymous") ;
+		const linkTitle = document.createElement("a");
+		linkTitle.textContent = this.getAttribute("title");
+		const newsContent = document.createElement("p");
+		newsContent.textContent =  this.getAttribute("content");
+		cardLeft.appendChild(autor);
+		cardLeft.appendChild(linkTitle);
+		cardLeft.appendChild(newsContent);
+
+		const cardRight = document.createElement("div");
+		const newsImage = document.createElement("img");
+
+		cardRight.setAttribute("class", "card__right");
+		cardRight.appendChild(newsImage);
+
+		componentRoot.appendChild(cardLeft);
+		componentRoot.appendChild(cardRight);
+		
+		return componentRoot;
 	}
+
 
 	styles(){ 
 	}
